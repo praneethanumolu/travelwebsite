6fbbd3e472a390d805dfdcd0af6e3f4efc26982f
praneeth <41447446+praneethanumolu@users.noreply.github.com>
2018-08-03 02:49:44 +0530

    latest

---
+++ Travel Website/Controllers/StaticPageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Travel_Website.Controllers
+{
+    public class StaticPageController : Controller
+    {
+        // GET: StaticPage
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
